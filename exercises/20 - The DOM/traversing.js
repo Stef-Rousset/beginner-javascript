710a4d4c0dcc99865a9b9113d56262cd0e5e3995
stephanie rousset <strabon@club-internet.fr>
2020-12-08 11:07:58 +0100

    training

---
+++ exercises/20 - The DOM/traversing.js
@@ -0,0 +1,7 @@
+console.log('it works');
+
+const header = document.querySelector('.header');
+console.log(header.children);
+console.log(header.lastElementChild);
+console.log(header.nexttElementSibling);
+console.log(header.parentElement);
