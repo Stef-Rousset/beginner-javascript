22bc6d9de2421421ea6924b124d6a511041ab9fd
stephanie rousset <strabon@club-internet.fr>
2021-01-22 12:51:03 +0100

    exo done

--- exercises/76 - Dad Jokes/jokes.js
+++ exercises/76 - Dad Jokes/jokes.js
@@ -8,3 +8,33 @@ const buttonText = [
   'please stop',
   'that was the worst one',
 ];
+const endpoint = 'https://icanhazdadjoke.com/'
+const jokeButton = document.querySelector('.getJoke');
+const jokeText = document.querySelector('.joke p');
+
+async function fetchJoke(){
+  const response = await fetch(endpoint, {
+    headers: {
+      Accept: 'application/json'
+    }
+  });
+  const data = await response.json();
+  return data.joke;
+  // dans la data retournée il y a une clé joke qui contient le texte de la blague
+}
+
+
+async function handleClick(){
+   const joke = await fetchJoke();
+   jokeText.textContent = joke;
+   jokeButton.textContent = randomItemArray(buttonText, jokeButton.textContent);
+}
+jokeButton.addEventListener('click', handleClick);
+
+function randomItemArray(array, not){
+  const item = array[Math.floor(Math.random() * array.length)];
+  if (item === not){
+    return randomItemArray(array,not);
+  }
+  return item;
+}
